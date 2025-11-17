96aa60dd68de07c5fec6f98e4af9d0092d4e6e92
Eufrasio Arruda Neto <eufrasioarrudaneto@gmail.com>
2025-11-17 16:01:40 -0300

    Add files via upload

---
+++ lista9/carrinho.h
@@ -0,0 +1,27 @@
+#ifndef CARRINHO_H_INCLUDED
+#define CARRINHO_H_INCLUDED
+
+typedef struct produto {
+     char nome[20];
+     char categoria[15];
+     int quantidade;
+     float valor;
+} Produto;
+
+typedef struct no {
+     Produto prod;
+     struct no *ant;
+     struct no *prox;
+} No;
+
+typedef struct lista {
+     No *inicio;
+     No *fim;
+     int tamanho;
+} ListaProdutos;
+
+void CriarCarrinho(ListaProdutos *car);
+void RetornarTam(ListaProdutos *car);
+void CarrinhoVazio(ListaProdutos *car);
+
+#endif // CARRINHO_H_INCLUDED
